9810e5995f38cb824102c4c3ddf42f543dac7a8b
MEBARKI16 <medaminemebarki@gmail.com>
2024-10-07 09:22:24 +0100

    update

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
-"use client";
+"use client"; // Indique que ce composant est rendu côté client
+
 import { useTranslation } from "react-i18next"; // Pour gérer les traductions
 import Link from "next/link";
 import { useState } from "react";
@@ -6,13 +7,18 @@ import styles from "./Navbar.module.css"; // CSS spécifique à la Navbar
 import { FaUserCircle } from "react-icons/fa"; // Icône d'utilisateur
 
 const Navbar = () => {
-    const { t } = useTranslation(); // Utiliser les traductions
+    const { t, i18n } = useTranslation();
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // Gérer l'ouverture du menu mobile
-
+    // Fonction pour ouvrir/fermer le menu mobile
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
     };
 
+    // Fermer le menu lors du changement de page
+    const handleLinkClick = () => {
+        setIsMobileMenuOpen(false); // Fermer le menu
+    };
+    const isArabic = i18n.language === 'ar';
     return (
         <nav className={styles.navbar}>
             <div className={styles.logo}>
@@ -21,39 +27,42 @@ const Navbar = () => {
                 </Link>
             </div>
 
-            <div className={`${styles.menu} ${isMobileMenuOpen ? styles.open : ""}`}>
+            <div
+                className={`${styles.menu} ${isMobileMenuOpen ? styles.open : ""} ${isArabic ? styles.menuArabic : ""
+                    }`}
+            >
                 <ul>
                     {/* Lien vers la page d'accueil */}
                     <li>
-                        <Link href="/">
+                        <Link href="/" onClick={handleLinkClick}>
                             {t("home")}
                         </Link>
                     </li>
 
                     {/* Lien vers la page "Qui sommes-nous" */}
                     <li>
-                        <Link href="/QuiSommesNous">
+                        <Link href="/QuiSommesNous" onClick={handleLinkClick}>
                             {t("qui_sommes_nous")}
                         </Link>
                     </li>
 
                     {/* Lien vers la page "Contact" */}
                     <li>
-                        <Link href="/Contact">
+                        <Link href="/Contact" onClick={handleLinkClick}>
                             {t("contact")}
                         </Link>
                     </li>
 
                     {/* Lien vers la page "Politique de confidentialité" */}
                     <li>
-                        <Link href="/PolitiqueConfidentialite">
+                        <Link href="/PolitiqueConfidentialite" onClick={handleLinkClick}>
                             {t("politique_confidentialite")}
                         </Link>
                     </li>
 
                     {/* Lien vers la page d'inscription */}
                     <li>
-                        <Link href="/inscription">
+                        <Link href="/inscription" onClick={handleLinkClick}>
                             {t("inscription")}
                         </Link>
                     </li>
@@ -61,7 +70,7 @@ const Navbar = () => {
             </div>
 
             <div className={styles.userIcon}>
-                <Link href="/compte">
+                <Link href="/compte" onClick={handleLinkClick}>
                     <FaUserCircle size={30} />
                 </Link>
             </div>
