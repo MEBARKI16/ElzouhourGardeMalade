bf3abe1e8815500ddec62ff9585e03f0364e0a0e
MEBARKI16 <medaminemebarki@gmail.com>
2024-10-07 12:07:14 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from 'next-pwa'; // Utilisation de l'import ES
 
-export default nextConfig;
+const withPWA = nextPWA({
+  dest: 'public', // Dossier où le service worker sera généré
+  disable: process.env.NODE_ENV === 'development', // Désactive le PWA en mode développement
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig); // Exporter la configuration
